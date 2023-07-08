2331db6e95a3f18b90e52378ae0ad458688b58ec
Adam Olszewski <adamolszewski96@gmail.com>
2023-07-08 10:23:17 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,7 @@ function App(){
     return (
     <div>
 <Header />
-{notes.map((note)=>{
-    return (
-        <Note
-            key={note.key}
-            title={note.title}
-            content={note.content}
-        />
-    )
-})}
+{notes.map((note)=><Note key={note.key} title={note.title} content={note.content} />)}
 <Footer />
 </div>
 )
