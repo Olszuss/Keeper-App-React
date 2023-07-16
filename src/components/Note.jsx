27b5ab33bfe9236b9236d071f9e2a0b48377f299
Adam Olszewski <adamolszewski96@gmail.com>
2023-07-16 19:53:25 +0200

    Update Note.jsx

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Note(props){
+
+
+const [pass, setPass] = useState(false)
+
+function isClicked(){
+    if (pass){
+        setPass(false)
+    }else {
+        setPass(true)
+    }
+}
+
+
     return (
-        <div className="note">
+        <div onClick={isClicked} style={{cursor: "pointer"}} className="note">
             <h1>{props.title}</h1>
-            <p>{props.content}</p>
+            <p style={{textDecoration: pass ? "line-through" : "none"}}>{props.content}</p>
         </div>
     )
 }
